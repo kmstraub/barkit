a1284bc1514b87d0b693f242e841dab7f903e274
kmstraub <kelly.m.straub@gmail.com>
2013-11-13 17:16:49 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Barkit::Application.config.secret_key_base = '0174b95147a6b7821a1434cb2fc98300e55b0a3ff242035ae3a80d20b817401aee6afe86f73b554d2baebf9cbd7e8b92d742fe0c473dec20fdde132538ee3392'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Barkit::Application.config.session_store :cookie_store, key: '_barkit_session'
